948aa9ab2abf465ba5eedf0f9139ab0799aa2dfe
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-09-16 17:09:39 -0300

    Add files via upload

---
+++ ano3000.py
@@ -0,0 +1,25 @@
+print('Em que ano voce nasceu?')
+nascimento = input()
+nascimento = int(nascimento)
+print('Para qual ano voce quer saber sua idade?')
+ano = input()
+ano = int(ano)
+idade_em = ano - nascimento
+print( "No ano", ano ,"voce tera ", idade_em , "anos!" )
+
+
+
+
+
+                
+
+
+
+
+
+
+
+
+       
+
+
