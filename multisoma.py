3e15a9cfdd81c01843723e4a21d79a2ec620e141
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-09-19 17:48:10 -0300

    Add files via upload

---
+++ multisoma.py
@@ -0,0 +1,29 @@
+n1 = float(input().strip())
+n2 = float(input().strip())
+soma = n1 + n2
+multiplicacao = n1 * n2
+print(soma)
+print(multiplicacao)
+
+
+
+
+
+
+         
+         
+         
+         
+         
+
+
+
+
+
+
+
+
+
+      
+      
+      
