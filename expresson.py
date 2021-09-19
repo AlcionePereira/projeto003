52e808939e9a2ecb773c90f5b23ce027dafb78f0
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-09-19 17:47:35 -0300

    Add files via upload

---
+++ expresson.py
@@ -0,0 +1,27 @@
+#Escreva um programa que leia o valor da variável x. Calcule e mostre o resultado da expressão:
+#    9x - 4x + 10 
+
+
+
+x = float(input().strip())
+a = 9 * x
+b = 4 * x
+c = a - b + 10
+print(c)
+
+
+
+
+          
+          
+
+
+
+
+
+
+
+          
+          
+          
+          
